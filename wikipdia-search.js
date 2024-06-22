c817d2bca1f21d92820713502197b10bee9e12e0
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-06-22 13:35:48 +0200

    refactor: re-organize

--- wikipdia-search.js
+++ wikipdia-search.js
@@ -16,35 +16,40 @@ function httpRequest(url) {
 function run(argv) {
 	const query = argv[0];
 	if (!query) return;
-    const encodedQuery = encodeURIComponent(query);
-    const langCodes = $.getenv("language_code").split('/');
-    const maxResults = Number($.getenv("max_number_of_result_for_each_language"));
+	const encodedQuery = encodeURIComponent(query);
+
+	const langCodes = $.getenv("language_code").split(/ *, */);
+	const maxResults = Number($.getenv("results_per_language"));
 	const useWikiwand = $.getenv("use_wikiwand") === "1";
 
-	// Wikiepdia Open Search API: https://www.mediawiki.org/wiki/API:Opensearch#JavaScript
-	// API Sandbox: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=opensearch&format=json&search=Hampi&namespace=0&limit=10&formatversion=2
+	// DOCS Wikipedia Open Search API: https://www.mediawiki.org/wiki/API:Opensearch#JavaScript
+	// TEST API Sandbox: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=opensearch&format=json&search=Hampi&namespace=0&limit=10&formatversion=2
 
 	/** @type AlfredItem[] */
-    const wikipediaEntries = [];
+	const wikiEntries = [];
 
-    for (const lang of langCodes) {
-		const wikipediaApiCall = `https://${lang}.wikipedia.org/w/api.php?action=opensearch&format=json&search=${encodedQuery}&namespace=0&limit=${maxResults}&profile=fuzzy`;
-		const wikipediaItems = JSON.parse(httpRequest(wikipediaApiCall));
+	for (const lang of langCodes) {
+		const apiCall = `https://${lang}.wikipedia.org/w/api.php?action=opensearch&format=json&search=${encodedQuery}&namespace=0&limit=${maxResults}&profile=fuzzy`;
+		const wikiItems = JSON.parse(httpRequest(apiCall));
 
-		for (let i = 0; i < wikipediaItems[1].length; i++) {
-			const suggestion = wikipediaItems[1][i];
-			const desc = wikipediaItems[2][i];
-			let url = wikipediaItems[3][i];
+		for (let i = 0; i < wikiItems[1].length; i++) {
+			const suggestion = wikiItems[1][i];
+			const desc = wikiItems[2][i]; // often empty
+			let url = wikiItems[3][i];
 			if (useWikiwand) url = url.replace(/.*\/wiki\/(.+)/, `https://www.wikiwand.com/${lang}/$1`);
 
-			wikipediaEntries.push({
+			// only show languages if actually more than one
+			const langDisplay = desc ? `[${lang}]` : lang;
+			const subtitle = langCodes.length > 1 ? `${langDisplay}  ${desc}` : desc;
+
+			wikiEntries.push({
 				title: suggestion,
-					subtitle: `In language: ‘${lang}’. ${desc}`,
+				subtitle: subtitle,
 				quicklookurl: url, // used by AlfredExtraPane
 				arg: url,
 			});
 		}
-    }
+	}
 
-	return JSON.stringify({ items: wikipediaEntries });
+	return JSON.stringify({ items: wikiEntries });
 }
