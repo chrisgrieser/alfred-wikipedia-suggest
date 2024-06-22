fb67a5a4242d16c341a5e73bf7988317b16873be
ユーザー名 <definitely_not_my@email.com>
2024-06-22 13:43:00 +0800

    Feat: configualble number of results. multiple languages

--- wikipdia-search.js
+++ wikipdia-search.js
@@ -16,32 +16,35 @@ function httpRequest(url) {
 function run(argv) {
 	const query = argv[0];
 	if (!query) return;
-
-	const lang = $.getenv("language_code");
+    const encodedQuery = encodeURIComponent(query);
+    const langCodes = $.getenv("language_code").split('/');
+    const maxResults = Number($.getenv("max_number_of_result_for_each_language"));
 	const useWikiwand = $.getenv("use_wikiwand") === "1";
 
 	// Wikiepdia Open Search API: https://www.mediawiki.org/wiki/API:Opensearch#JavaScript
 	// API Sandbox: https://en.wikipedia.org/wiki/Special:ApiSandbox#action=opensearch&format=json&search=Hampi&namespace=0&limit=10&formatversion=2
-	const maxResults = 9; // Alfred only shows 9 items at once
-	const encodedQuery = encodeURIComponent(query);
-	const wikipediaApiCall = `https://${lang}.wikipedia.org/w/api.php?action=opensearch&format=json&search=${encodedQuery}&namespace=0&limit=${maxResults}&profile=fuzzy`;
-	const wikipediaItems = JSON.parse(httpRequest(wikipediaApiCall));
 
 	/** @type AlfredItem[] */
-	const wikipediaEntries = [];
-	for (let i = 0; i < wikipediaItems[1].length; i++) {
-		const suggestion = wikipediaItems[1][i];
-		const desc = wikipediaItems[2][i];
-		let url = wikipediaItems[3][i];
-		if (useWikiwand) url = url.replace(/.*\/wiki\/(.+)/, `https://www.wikiwand.com/${lang}/$1`);
+    const wikipediaEntries = [];
 
-		wikipediaEntries.push({
-			title: suggestion,
-			subtitle: desc,
-			quicklookurl: url, // used by AlfredExtraPane
-			arg: url,
-		});
-	}
+    for (const lang of langCodes) {
+		const wikipediaApiCall = `https://${lang}.wikipedia.org/w/api.php?action=opensearch&format=json&search=${encodedQuery}&namespace=0&limit=${maxResults}&profile=fuzzy`;
+		const wikipediaItems = JSON.parse(httpRequest(wikipediaApiCall));
+
+		for (let i = 0; i < wikipediaItems[1].length; i++) {
+			const suggestion = wikipediaItems[1][i];
+			const desc = wikipediaItems[2][i];
+			let url = wikipediaItems[3][i];
+			if (useWikiwand) url = url.replace(/.*\/wiki\/(.+)/, `https://www.wikiwand.com/${lang}/$1`);
+
+			wikipediaEntries.push({
+				title: suggestion,
+					subtitle: `In language: ‘${lang}’. ${desc}`,
+				quicklookurl: url, // used by AlfredExtraPane
+				arg: url,
+			});
+		}
+    }
 
 	return JSON.stringify({ items: wikipediaEntries });
 }
